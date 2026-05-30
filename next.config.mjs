975bd89e86320587c536739cfca582b653106e5b
yitian.chen <chen1tian@hotmail.com>
2026-05-30 18:02:08 +0800

    禁用 SWC 压缩

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: false, // 禁用 SWC 压缩，在 Android/arm64 上 SWC 二进制不可用
+};
 
 export default nextConfig;
